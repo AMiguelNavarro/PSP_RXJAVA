1dc7604903fb2714ff8068177f60544159cc84c2
Mike <alberto.miguel96@gmail.com>
2021-03-07 21:24:52 +0100

    Se muestra todo en un ListView pero sin RXJava

--- src/main/java/com/sanvalero/controller/AppController.java
+++ src/main/java/com/sanvalero/controller/AppController.java
@@ -1,27 +1,33 @@
 package com.sanvalero.controller;
 
 import com.sanvalero.domain.Country;
-import com.sanvalero.service.CountriesServiceImplementation;
+import com.sanvalero.service.CountriesService;
+import javafx.collections.FXCollections;
 import javafx.event.Event;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import rx.Observable;
+import rx.schedulers.Schedulers;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.concurrent.Executors;
 
 public class AppController implements Initializable {
 
     public Button btListAll;
     public ListView lvList;
-    CountriesServiceImplementation service;
+    CountriesService service;
+
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        service = new CountriesServiceImplementation();
+        service = new CountriesService();
     }
 
 
@@ -30,9 +36,20 @@ public class AppController implements Initializable {
 
         System.out.println("Funciona");
 
+        lvList.getItems().clear();
+
         List<Country> allCountries = service.getAllCountries();
 
-//        System.out.println(allCountries.toString()); /* Pinta todas correctamente por consola */
+        lvList.setItems(FXCollections.observableList(allCountries));
+
+//        service.getAllCountries()
+//                .flatMap(Observable::from)
+//                .doOnCompleted(() -> System.out.println("Listado de países descargado"))
+//                .doOnError(throwable -> System.out.println(throwable.getMessage()))
+//                .subscribeOn(Schedulers.from(Executors.newCachedThreadPool()))
+//                .subscribe(lvList.setItems(FXCollections.observableList()));
+
+        //System.out.println(allCountries.toString()); /* Pinta todas correctamente por consola */
 
     }
 
